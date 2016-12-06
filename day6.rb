b6ffff0a7e5870b0c3d3aa61d6e33ef727bde02f
Jonathon Fry <jonfry22@gmail.com>
2016-12-06 08:47:35 +0000

    Day 6 Part 1

---
+++ day6.rb
@@ -0,0 +1,18 @@
+contents = File.read('day6.txt').strip
+
+data = Hash.new(Hash.new)
+
+contents.each_line do |line|
+    line.strip.chars.map.with_index do |c, i|
+        indexMap = data.fetch(i, Hash.new(0))
+        indexMap[c] += 1
+        data[i] = indexMap
+    end
+end
+
+password = ""
+data.each do |key, value|
+    data[key] = value.sort_by{|x,y| -y}.to_h
+    password += data[key].keys[0]
+end
+puts password
