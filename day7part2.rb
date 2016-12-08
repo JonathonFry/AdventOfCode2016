d3741f256561de35693ca74a3677474d785ad29e
Jonathon Fry <j.fry@waracle.com>
2016-12-08 11:52:16 +0000

    Removed test comments

--- day7part2.rb
+++ day7part2.rb
@@ -1,14 +1,5 @@
 contents = File.read('day7.txt').strip
 
-
-# An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, anywhere in the supernet sequences (outside any square bracketed sections), 
-# and a corresponding Byte Allocation Block, or BAB, anywhere in the hypernet sequences. 
-# An ABA is any three-character sequence which consists of the same character twice with a different character between them, such as xyx or aba. 
-# A corresponding BAB is the same characters but in reversed positions: yxy and bab, respectively.
-
-# aba[bab]xyz supports SSL (aba outside square brackets with corresponding bab within square brackets).
-
-
 def isAba(string)
     abas = Array.new
     (0).upto(string.length - 1) do |n|
@@ -70,6 +61,3 @@ contents.each_line do |line|
 end
 
 puts validIps
-
-# 19 = too low
-# 364 = too high
